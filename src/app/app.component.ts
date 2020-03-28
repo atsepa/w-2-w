9e488483da1bb7dd0745bb90da032590e80bcc67
Francisco Garcia <beickon@franciscos-mbp.myfiosgateway.com>
2020-03-28 11:45:19 -0400

    feature(general): Add streaming component and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StreamingService } from './services/streaming.service';
 
 @Component({
 	selector: 'app-root',
@@ -6,7 +7,10 @@ import { Component } from '@angular/core';
 	styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
+	constructor(private streamingService: StreamingService) {}
+
 	handleClick() {
 		console.log('cliiiick');
+		this.streamingService.streamingEvent.emit();
 	}
 }
